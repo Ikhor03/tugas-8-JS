dce6687a4e4c57aca0bba6d623e3259d8e665e5b
Ikhor03 <imamkhoiri725@gmail.com>
2023-01-13 13:52:00 +0700

    task8

---
+++ tugas8.js
@@ -0,0 +1,14 @@
+const biodata = {
+    nickName: "Ikhor",
+    from: "Blitar",
+    age: 23,
+    domisili: "Jakarta",
+    hobi: "menggambar",
+}
+console.log(biodata);
+
+biodata.hobi = "Mendengarkan Musik";
+console.log(biodata);
+
+biodata.bekerja = false;
+console.log(biodata);
